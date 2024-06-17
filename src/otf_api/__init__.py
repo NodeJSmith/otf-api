9b11c1ca75e29e34e48560f029d59564ea90e44f
Jessica Smith <12jessicasmith34@gmail.com>
2024-06-16 20:37:02 -0500

    refactor(init.py): remove unused imports and simplify __all__ list to improve code maintainability

--- src/otf_api/__init__.py
+++ src/otf_api/__init__.py
@@ -6,68 +6,11 @@ from loguru import logger
 from . import classes_api, member_api, studios_api, telemetry_api
 from .api import Api
 from .models.auth import User
-from .models.responses import (
-    BookingList,
-    BookingStatus,
-    ChallengeTrackerContent,
-    ChallengeTrackerDetailList,
-    ChallengeType,
-    EquipmentType,
-    FavoriteStudioList,
-    HistoryClassStatus,
-    LatestAgreement,
-    MemberDetail,
-    MemberMembership,
-    MemberPurchaseList,
-    OtfClassList,
-    OutOfStudioWorkoutHistoryList,
-    PerformanceSummaryDetail,
-    PerformanceSummaryList,
-    StudioDetail,
-    StudioDetailList,
-    StudioServiceList,
-    Telemetry,
-    TelemetryHrHistory,
-    TelemetryMaxHr,
-    TotalClasses,
-    WorkoutList,
-)
 
 __version__ = "0.2.2"
 
 
-__all__ = [
-    "Api",
-    "User",
-    "member_api",
-    "BookingList",
-    "ChallengeTrackerContent",
-    "ChallengeTrackerDetailList",
-    "ChallengeType",
-    "BookingStatus",
-    "EquipmentType",
-    "HistoryClassStatus",
-    "LatestAgreement",
-    "MemberDetail",
-    "MemberMembership",
-    "MemberPurchaseList",
-    "OutOfStudioWorkoutHistoryList",
-    "StudioServiceList",
-    "TotalClasses",
-    "WorkoutList",
-    "FavoriteStudioList",
-    "OtfClassList",
-    "classes_api",
-    "studios_api",
-    "telemetry_api",
-    "TelemetryHrHistory",
-    "Telemetry",
-    "TelemetryMaxHr",
-    "StudioDetail",
-    "StudioDetailList",
-    "PerformanceSummaryDetail",
-    "PerformanceSummaryList",
-]
+__all__ = ["Api", "User", "member_api", "studios_api", "classes_api", "telemetry_api"]
 
 logger.remove()
 logger.add(sink=sys.stdout, level=os.getenv("OTF_LOG_LEVEL", "INFO"))
