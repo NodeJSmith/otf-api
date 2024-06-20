ffc1cee14e664ae97b45bbea3d9d8ce7d0888640
Jessica Smith <12jessicasmith34@gmail.com>
2024-06-20 07:31:58 -0500

    feat(classes_api): add class_type and exclude_cancelled filters to get_classes method fix(classes_api): correct variable name from res to classes_resp for clarity feat(classes_api): integrate booking status to mark classes as booked

--- src/otf_api/classes_api.py
+++ src/otf_api/classes_api.py
@@ -1,7 +1,8 @@
 import typing
 from datetime import datetime
 
-from otf_api.models.responses.classes import OtfClassList
+from otf_api.models.responses.bookings import BookingStatus
+from otf_api.models.responses.classes import ClassType, OtfClassList
 
 if typing.TYPE_CHECKING:
     from otf_api import Api
@@ -23,6 +24,8 @@ class ClassesApi:
         start_date: str | None = None,
         end_date: str | None = None,
         limit: int | None = None,
+        class_type: ClassType | None = None,
+        exclude_cancelled: bool = True,
     ) -> OtfClassList:
         """Get the classes for the user.
 
@@ -36,6 +39,8 @@ class ClassesApi:
             start_date (str | None): The start date to get classes for, in the format "YYYY-MM-DD". Default is None.
             end_date (str | None): The end date to get classes for, in the format "YYYY-MM-DD". Default is None.
             limit (int | None): Limit the number of classes returned. Default is None.
+            class_type (ClassType | None): The class type to filter by. Default is None.
+            exclude_cancelled (bool): Whether to exclude cancelled classes. Default is True.
 
         Returns:
             OtfClassList: The classes for the user.
@@ -49,8 +54,9 @@ class ClassesApi:
         path = "/v1/classes"
 
         params = {"studio_ids": studio_uuids}
-        res = await self._api._classes_request("GET", path, params=params)
-        classes_list = OtfClassList(classes=res["items"])
+
+        classes_resp = await self._api._classes_request("GET", path, params=params)
+        classes_list = OtfClassList(classes=classes_resp["items"])
 
         if start_date:
             start_dtme = datetime.strptime(start_date, "%Y-%m-%d")  # noqa
@@ -63,7 +69,21 @@ class ClassesApi:
         if limit:
             classes_list.classes = classes_list.classes[:limit]
 
+        if class_type:
+            classes_list.classes = [c for c in classes_list.classes if c.class_type == class_type]
+
+        if exclude_cancelled:
+            classes_list.classes = [c for c in classes_list.classes if not c.canceled]
+
         for otf_class in classes_list.classes:
             otf_class.is_home_studio = otf_class.studio.id == self._api.home_studio.studio_uuid
+
+        classes_list.classes = list(filter(lambda c: not c.canceled, classes_list.classes))
+
+        booking_resp = await self._api.member_api.get_bookings(start_date, end_date, status=BookingStatus.Booked)
+        booked_classes = {b.otf_class.class_uuid for b in booking_resp.bookings}
+
+        for otf_class in classes_list.classes:
+            otf_class.is_booked = otf_class.ot_class_uuid in booked_classes
 
         return classes_list
