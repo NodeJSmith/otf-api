eb458ff757b6512a355a05eebfc7e0b933b9319e
Jessica Smith <12jessicasmith34@gmail.com>
2024-07-18 21:12:35 -0500

    fix test

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,7 +2,6 @@ import pytest
 from otf_api.api import Otf
 
 
-@pytest.mark.asyncio
-async def test_api_raises_error_if_no_username_password():
-    with pytest.raises(TypeError):
-        await Otf.create()
+def test_api_raises_error_if_no_username_password():
+    with pytest.raises(ValueError):
+        Otf()
