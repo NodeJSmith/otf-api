e1c930ac1a5b721197080ee52ffec7f547953d1d
Jessica Smith <12jessicasmith34@gmail.com>
2024-12-23 15:49:31 -0600

    bump dev

--- src/otf_api/__init__.py
+++ src/otf_api/__init__.py
@@ -8,7 +8,7 @@ DATE_FMT = "%Y-%m-%d %H:%M:%S%z"
 
 basicConfig(level="INFO", style="{", format=LOG_FMT, datefmt=DATE_FMT)
 
-__version__ = "0.8.3-dev2"
+__version__ = "0.8.3-dev3"
 
 
 __all__ = ["Otf", "OtfUser"]
