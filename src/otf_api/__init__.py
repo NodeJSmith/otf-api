7e1515147fb94460d4dff156aa87f2ccb686ea51
Jessica Smith <12jessicasmith34@gmail.com>
2024-12-23 16:54:07 -0600

    refactor(logging): move logging configuration to a separate module for better organization and reusability

--- src/otf_api/__init__.py
+++ src/otf_api/__init__.py
@@ -1,15 +1,8 @@
-import logging
-from logging import basicConfig
+from otf_api import logging  # noqa # type: ignore
 
 from .api import Otf
 from .auth import OtfUser
 from .sync_api import OtfSync
-
-LOG_FMT = "{asctime} - {module}.{funcName}:{lineno} - {levelname} - {message}"
-DATE_FMT = "%Y-%m-%d %H:%M:%S%z"
-
-basicConfig(level="INFO", style="{", format=LOG_FMT, datefmt=DATE_FMT)
-logging.getLogger("httpx").setLevel(logging.WARNING)
 
 __version__ = "0.9.2"
 
