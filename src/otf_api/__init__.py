c2f6db907139280794186c988cc7d46e8a28f093
Jessica Smith <12jessicasmith34@gmail.com>
2024-12-23 16:40:16 -0600

    bump minor, new sync api

--- src/otf_api/__init__.py
+++ src/otf_api/__init__.py
@@ -9,7 +9,7 @@ DATE_FMT = "%Y-%m-%d %H:%M:%S%z"
 
 basicConfig(level="INFO", style="{", format=LOG_FMT, datefmt=DATE_FMT)
 
-__version__ = "0.8.3-dev4"
+__version__ = "0.9.0"
 
 
 __all__ = ["Otf", "OtfSync", "OtfUser"]
