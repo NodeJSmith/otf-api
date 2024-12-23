59b1e1b0953c6a4d880f39e641f70834ac955736
Jessica Smith <12jessicasmith34@gmail.com>
2024-12-23 15:49:22 -0600

    refactor(init.py): remove unused getLogger import to clean up code style(init.py): enhance logging format for better readability and consistency

--- src/otf_api/__init__.py
+++ src/otf_api/__init__.py
@@ -1,10 +1,12 @@
-from logging import basicConfig, getLogger
+from logging import basicConfig
 
 from .api import Otf
 from .auth import OtfUser
 
-basicConfig(level="INFO")
-logger = getLogger(__name__)
+LOG_FMT = "{asctime} - {module}.{funcName}:{lineno} - {levelname} - {message}"
+DATE_FMT = "%Y-%m-%d %H:%M:%S%z"
+
+basicConfig(level="INFO", style="{", format=LOG_FMT, datefmt=DATE_FMT)
 
 __version__ = "0.8.3-dev2"
 
