f8e1d53f629fa56459de39f69a816a91865afba1
Jessica Smith <12jessicasmith34@gmail.com>
2024-12-23 15:34:59 -0600

    feat(otf_api): add basic logging configuration to improve debugging and monitoring

--- src/otf_api/__init__.py
+++ src/otf_api/__init__.py
@@ -1,5 +1,10 @@
+from logging import basicConfig, getLogger
+
 from .api import Otf
 from .auth import OtfUser
+
+basicConfig(level="INFO")
+logger = getLogger(__name__)
 
 __version__ = "0.8.3-dev1"
 
