c0cbce307e47f40d8c09d59ebc2b548f0adf0de7
Jessica Smith <12jessicasmith34@gmail.com>
2024-12-23 16:39:54 -0600

    feat(examples_sync): add example scripts for challenge tracker, class bookings, studio, and workout functionalities
    Add example scripts to demonstrate the usage of the OtfSync API for
    various functionalities including challenge tracking, class bookings,
    studio information, and workout data. These examples serve as a guide
    for users to understand how to interact with the API and retrieve
    relevant data.
    
    feat(otf_api): introduce OtfSync class for synchronous API interactions
    
    Add a new OtfSync class to the otf_api module to facilitate
    synchronous interactions with the Orangetheory Fitness API. This class
    provides methods for booking classes, retrieving member details,
    accessing performance summaries, and more. It aims to simplify the
    process of making API requests and handling responses in a
    synchronous manner.

---
+++ examples_sync/studio_examples.py
@@ -0,0 +1,73 @@
+import os
+from getpass import getpass
+
+from otf_api import OtfSync
+
+USERNAME = os.getenv("OTF_EMAIL") or input("Enter your OTF email: ")
+PASSWORD = os.getenv("OTF_PASSWORD") or getpass("Enter your OTF password: ")
+
+
+def main():
+    with OtfSync(USERNAME, PASSWORD) as otf:
+        # if you need to figure out what studios are in an area, you can call `search_studios_by_geo`
+        # which takes latitude, longitude, distance, page_index, and page_size as arguments
+        # but you'll generally just need the first 3
+        # same as with classes, you can leave it blank and get the studios within 50 miles of your home studio
+        studios_by_geo = otf.search_studios_by_geo()
+        print(studios_by_geo.studios[0].model_dump_json(indent=4))
+
+        """
+        {
+            "studio_id": 1297,
+            "studio_uuid": "8645fb2b-ef66-4d9d-bda1-f508091ec891",
+            "mbo_studio_id": 8612481,
+            "studio_number": "05414",
+            "studio_name": "AnyTown OH - East",
+            "studio_physical_location_id": 494,
+            "time_zone": "America/Chicago",
+            "contact_email": "studiomanager05414@orangetheoryfitness.com",
+            "studio_token": "ec2459b2-32b5-4b7e-9759-55270626925a",
+            "environment": "PROD",
+            "pricing_level": "",
+            "tax_rate": "0.000000",
+            "accepts_visa_master_card": true,
+            "accepts_american_express": true,
+            "accepts_discover": true,
+            "accepts_ach": false,
+            "is_integrated": true,
+            "description": "",
+            "studio_version": "",
+            "studio_status": "Active",
+            "open_date": "2017-01-13 00:00:00",
+            "re_open_date": "2020-05-26 00:00:00",
+            "studio_type_id": 2,
+            "sms_package_enabled": false,
+            "allows_dashboard_access": false,
+            "allows_cr_waitlist": true,
+            "cr_waitlist_flag_last_updated": "2020-07-09 02:43:55+00:00",
+            "royalty_rate": 0,
+            "marketing_fund_rate": 0,
+            "commission_percent": 0,
+            "is_mobile": null,
+            "is_otbeat": null,
+            "distance": 0.0,
+            "studio_location": ...,
+            "studio_location_localized": ...,
+            "studio_profiles": {
+                "is_web": true,
+                "intro_capacity": 1,
+                "is_crm": true
+            },
+            "social_media_links": ...
+        }
+        """
+
+        # if you need to get detailed information about a studio, you can call `get_studio_detail`
+        # which takes a studio_uuid as an argument, but you can leave it blank to get details about your home studio
+        # this one has a result structure very much like the previous one
+        studio_detail = otf.get_studio_detail()
+        print(studio_detail.model_dump_json(indent=4))
+
+
+if __name__ == "__main__":
+    main()
--- src/otf_api/__init__.py
+++ src/otf_api/__init__.py
@@ -2,6 +2,7 @@ from logging import basicConfig
 
 from .api import Otf
 from .auth import OtfUser
+from .sync_api import OtfSync
 
 LOG_FMT = "{asctime} - {module}.{funcName}:{lineno} - {levelname} - {message}"
 DATE_FMT = "%Y-%m-%d %H:%M:%S%z"
@@ -11,4 +12,4 @@ basicConfig(level="INFO", style="{", format=LOG_FMT, datefmt=DATE_FMT)
 __version__ = "0.8.3-dev4"
 
 
-__all__ = ["Otf", "OtfUser"]
+__all__ = ["Otf", "OtfSync", "OtfUser"]
