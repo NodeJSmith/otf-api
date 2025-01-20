af81abb2b473f1fc6a48d8424ea0d242e19981a6
Jessica Smith <12jessicasmith34@gmail.com>
2025-01-20 14:01:43 -0600

    add option to change log level

--- src/otf_api/logging.py
+++ src/otf_api/logging.py
@@ -1,4 +1,7 @@
 import logging
+import os
+
+LOG_LEVEL = os.getenv("OTF_LOG_LEVEL", "INFO")
 
 LOG_FMT = "{asctime} - {module}.{funcName}:{lineno} - {levelname} - {message}"
 DATE_FMT = "%Y-%m-%d %H:%M:%S%z"
@@ -6,7 +9,7 @@ DATE_FMT = "%Y-%m-%d %H:%M:%S%z"
 logger = logging.getLogger("otf_api")
 
 # 2) Set the logger level to INFO (or whatever you need).
-logger.setLevel(logging.INFO)
+logger.setLevel(LOG_LEVEL)
 
 # 3) Create a handler (e.g., console) and set its formatter.
 handler = logging.StreamHandler()
