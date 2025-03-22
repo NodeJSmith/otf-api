a94f1f41d96cc965841c186bdd6efa65ab9636b5
Jessica Smith <12jessicasmith34@gmail.com>
2025-03-21 19:15:34 -0500

    bump version after fixing studio detail 404

--- src/otf_api/__init__.py
+++ src/otf_api/__init__.py
@@ -4,7 +4,7 @@ from otf_api.api import Otf
 from otf_api import models
 from otf_api.auth import OtfUser
 
-__version__ = "0.10.0"
+__version__ = "0.10.1"
 
 
 __all__ = ["Otf", "OtfUser", "models"]
