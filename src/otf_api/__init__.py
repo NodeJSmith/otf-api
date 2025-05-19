cbb03427c65a0a4529765ae173c783748b2f5def
Jessica Smith <12jessicasmith34@gmail.com>
2025-05-19 13:51:09 -0500

    minor version increase due to changed arguments

--- src/otf_api/__init__.py
+++ src/otf_api/__init__.py
@@ -4,7 +4,7 @@ from otf_api.api import Otf
 from otf_api import models
 from otf_api.auth import OtfUser
 
-__version__ = "0.11.1"
+__version__ = "0.12.0"
 
 
 __all__ = ["Otf", "OtfUser", "models"]
