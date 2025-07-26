7fb9d7314f82370cef26c0cb97be66717f19b5cb
Jessica Smith <12jessicasmith34@gmail.com>
2025-07-26 14:27:00 -0500

    add/update logging, use % formatting to avoid wasted string creation, add debug log for return value

--- src/otf_api/api/client.py
+++ src/otf_api/api/client.py
@@ -1,4 +1,5 @@
 import atexit
+import json
 import re
 from json import JSONDecodeError
 from logging import getLogger
@@ -110,7 +111,7 @@ class OtfClient:
         """
         full_url = str(URL.build(scheme="https", host=base_url, path=path))
         request = self._build_request(method, full_url, params, headers, **kwargs)
-        LOGGER.debug(f"Making {method!r} request to '{full_url}', params: {params}, headers: {headers}")
+        LOGGER.debug("Making %r request to '%s', params: %s, headers: %s", method, full_url, params, headers)
 
         try:
             response = self.session.send(request)
@@ -158,10 +159,14 @@ class OtfClient:
             if error_code == "602":
                 raise exc.OutsideSchedulingWindowError("Class is outside scheduling window")
 
-        msg = f"HTTP error {error.response.status_code} for {request.method} {request.url}"
-        LOGGER.error(msg)
+        LOGGER.error("HTTP error %s for %s %s", response.status_code, request.method, request.url)
         error_cls = exc.RetryableOtfRequestError if response.status_code >= 500 else exc.OtfRequestError
-        raise error_cls(message=msg, original_exception=error, request=request, response=response)
+        raise error_cls(
+            message=f"HTTP error {response.status_code} for {request.method} {request.url}",
+            original_exception=error,
+            request=request,
+            response=response,
+        )
 
     def _handle_transport_error(self, error: Exception, request: httpx.Request) -> None:
         """Handle transport errors during API requests.
@@ -177,7 +182,7 @@ class OtfClient:
         url = request.url
 
         if not isinstance(error, httpx.HTTPStatusError):
-            LOGGER.exception(f"Unexpected error during {method!r} {url!r}: {type(error).__name__} - {error}")
+            LOGGER.exception("Unexpected error during %r %r: %s - %s", method, url, type(error).__name__, error)
             return
 
         json_data = get_json_from_response(error.response)
@@ -190,7 +195,7 @@ class OtfClient:
         data_status: int | None = data.get("Status") or data.get("status") or None
 
         if isinstance(data, dict) and isinstance(data_status, int) and not 200 <= data_status <= 299:
-            LOGGER.error(f"API returned error: {data}")
+            LOGGER.error("API returned error: %s", data)
             raise exc.OtfRequestError("Bad API response", None, response=response, request=request)
 
         raise exc.OtfRequestError(
@@ -202,17 +207,19 @@ class OtfClient:
             if method == "GET":
                 raise exc.OtfRequestError("Empty response", None, response=response, request=request)
 
-            LOGGER.debug(f"No content returned from {method} {response.url}")
+            LOGGER.debug("No content returned from %s %s", method, response.url)
             return None
 
         try:
             json_data = response.json()
         except JSONDecodeError as e:
-            LOGGER.error(f"Invalid JSON: {e}")
-            LOGGER.error(f"Response content: {response.text}")
+            LOGGER.error("Invalid JSON: %s", e)
+            LOGGER.error("Response content: %s", response.text)
             raise
 
         if is_error_response(json_data):
             self._map_logical_error(json_data, response, request)
+
+        LOGGER.debug("Response from %s %s: %s", method, response.url, json.dumps(json_data, indent=4))
 
         return json_data
