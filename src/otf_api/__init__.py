5e0b38e99213e2b59765cf37d8fd03661e094481
Jessica Smith <12jessicasmith34@gmail.com>
2025-07-26 15:02:13 -0500

    add coloredlogs, change format style to %

--- src/otf_api/__init__.py
+++ src/otf_api/__init__.py
@@ -7,13 +7,15 @@ Use it at your own risk. It may break at any time if Orangetheory changes their 
 import logging
 import os
 
+import coloredlogs
+
 from otf_api import models
 from otf_api.api import Otf
 from otf_api.auth import OtfUser
 
 LOG_LEVEL = os.getenv("OTF_LOG_LEVEL", "INFO").upper()
-LOG_LEVEL_NUM = getattr(logging, LOG_LEVEL, logging.INFO)
-LOG_FMT = "{asctime} - {module}.{funcName}:{lineno} - {levelname} - {message}"
+
+LOG_FMT = "%(asctime)s - %(module)s.%(funcName)s:%(lineno)d - %(levelname)s - %(message)s"
 DATE_FMT = "%Y-%m-%d %H:%M:%S%z"
 
 
@@ -24,14 +26,23 @@ def _setup_logging() -> None:
         return  # Already set up
 
     # 2) Set the logger level to INFO (or whatever you need).
-    logger.setLevel(LOG_LEVEL_NUM)
+    logger.setLevel(LOG_LEVEL)
 
     # 3) Create a handler (e.g., console) and set its formatter.
     handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter(fmt=LOG_FMT, datefmt=DATE_FMT, style="{"))
+    handler.setFormatter(logging.Formatter(fmt=LOG_FMT, datefmt=DATE_FMT, style="%"))
 
     # 4) Add this handler to your package logger.
     logger.addHandler(handler)
+
+    coloredlogs.install(
+        level=LOG_LEVEL,
+        logger=logger,
+        fmt=LOG_FMT,
+        datefmt=DATE_FMT,
+        style="%",
+        isatty=True,  # Use colored output only if the output is a terminal
+    )
 
 
 _setup_logging()
