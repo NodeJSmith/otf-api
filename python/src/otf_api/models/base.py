37625a550c6feaaaa47bd633f5ac6bd3f55b1a53
Micah Huff <micah@micahhuff.com>
2025-08-26 05:58:42 -0700

    Minor UV fix, code validation will now pass

--- python/src/otf_api/models/base.py
+++ python/src/otf_api/models/base.py
@@ -5,8 +5,8 @@ from pydantic import BaseModel, ConfigDict
 
 class OtfItemBase(BaseModel):
     model_config: ClassVar[ConfigDict] = ConfigDict(
-        arbitrary_types_allowed=True, 
+        arbitrary_types_allowed=True,
         extra="ignore",
         # Use Python field names in schema generation (not validation_alias)
-        by_alias=False
+        by_alias=False,
     )
